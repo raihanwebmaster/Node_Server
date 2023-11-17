8820ed2d3d9903177f2f602e50be8ef5bc457e57
Raihan Uddin <raihanemon2015@gmail.com>
2023-11-17 08:37:03 +0100

    setup

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+/** @format */
+
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+const app: Application = express();
+
+// const port = 8000;
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+});
+
+export default app;
